051b9e328b050c26ef0bbdfddaa64dfc1f9223b9
arishahmad <arishahmad@localhost>
2022-07-21 21:11:15 +0530

    first commit

---
+++ sample-jenkins-project/src/main/java/com/first/SampleJenkinsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleJenkinsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleJenkinsProjectApplication.class, args);
+	}
+
+}
