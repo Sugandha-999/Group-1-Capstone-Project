efd093314369a9aeac0642d5e0eb66ce4ecc071c
arishahmad <arishahmad@localhost>
2022-07-22 20:54:16 +0530

    first commit

--- Eureka-discovery-wipro13-client/src/main/java/com/first/repo/EmpRepo.java
+++
@@ -1,9 +0,0 @@
-package com.first.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.first.entity.Employee;
-
-public interface EmpRepo extends JpaRepository<Employee,Integer> {
-
-}
